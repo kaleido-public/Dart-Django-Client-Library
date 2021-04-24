a583f9f72b7ef5e7120c42c075f9c0b161fba2d9
roychen7 <roychen0724@hotmail.com>
2021-04-24 01:16:47 -0700

    finished most of non axios related stuff

--- lib/src/CollectionManager.dart
+++ lib/src/CollectionManager.dart
@@ -5,9 +5,7 @@ import './AbstractCollectionManager.dart';
 class CollectionManager<T extends Model> extends AbstractCollectionManager {
   ItemCreator<T> creator;
 
-  CollectionManager(ItemCreator<T> this.creator) {
-    
-  }
+  CollectionManager(ItemCreator<T> this.creator) {}
 
   String get collection_url {
     return "/${T.toString().toLowerCase()}";
--- lib/src/RelatedCollectionManager.dart
+++ lib/src/RelatedCollectionManager.dart
@@ -1,20 +1,21 @@
 import './Model.dart';
 import './ItemCreatorType.dart';
+import './AbstractCollectionManager.dart';
 import 'dart:mirrors';
 
-class RelatedObjectManager<T extends Model, P extends Model> {
+class RelatedCollectionManager <T extends Model, P extends Model> extends AbstractCollectionManager<T> {
   int? parent_id;
   String? parent_key;
   String? parent_model_name;
-  ItemCreator<T>? creator;
-
-  RelatedObjectManager(ItemCreator<T> this.creator, P parent, String this.parent_key) {
+  ItemCreator<T> creator;
+  
+  RelatedCollectionManager(ItemCreator<T> this.creator, P parent, String this.parent_key) {
     this.parent_id = parent.id;
     InstanceMirror parent_mirror = reflect(parent);
     this.parent_model_name = parent_mirror.type.reflectedType.toString().toLowerCase();
   }
 
-  String get object_url {
+  String get collection_url {
     return '/${this.parent_model_name}/${this.parent_id}/${this.parent_key}';
   }
 }
